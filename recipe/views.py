3c9b37ae69688cdaf8b3a5bdc958cdadbbb22ff4
Letisha <chibi.panda93@gmail.com>
2015-08-22 12:26:41 +0200

    ListView
    Using class based views, they're awesome!

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from recipe.models import Recipe
 
-# Create your views here.
+
+class RecipeList(ListView):
+    """
+    Lists out the existing recipes.
+    """
+    model = Recipe
+    template_name = 'home.html'
