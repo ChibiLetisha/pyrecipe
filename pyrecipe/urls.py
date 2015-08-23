57495c0a9cb3d24bfb147cd38d39dd251236f673
Letisha <chibi.panda93@gmail.com>
2015-08-23 12:01:20 +0200

    Forms
    Registration form and New Recipe form available.

--- pyrecipe/urls.py
+++ pyrecipe/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from recipe.views import RecipeList, RecipeDetail
+from recipe.views import RecipeList, RecipeDetail, Register, NewRecipe
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', RecipeList.as_view(), name='home'),
+    url(r'^new-recipe/$', NewRecipe.as_view(), name='new-recipe'),
+    url(r'^register/$', Register.as_view(), name='register'),
     url(r'^recipe/(?P<slug>[-\w]+)/$', RecipeDetail.as_view(), name='recipe-detail'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,23 @@
+from django import forms
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse_lazy
+from django.forms import modelform_factory
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.utils.decorators import method_decorator
+from django.utils.text import slugify
+from django.views.generic import ListView, DetailView, CreateView, View
 from recipe.models import Recipe
+
+
+class LoginRequiredMixin(View):
+    """
+    Checks if user has been logged in.
+    Doesn't let unauthorized people access the page.
+    """
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 
 class RecipeList(ListView):
@@ -19,3 +36,43 @@ class RecipeDetail(DetailView):
     """
     model = Recipe
     template_name = 'recipe.html'
+
+
+class NewRecipe(LoginRequiredMixin, CreateView):
+    """
+    Saves recipe created by logged in user.
+    """
+    model = Recipe
+    template_name = 'register_form.html'
+    success_url = reverse_lazy('home')
+    fields = ['name', 'description']
+
+    def form_valid(self, form):
+        recipe = form.save(commit=False)
+        recipe.created_by = self.request.user
+        recipe.slug = slugify(recipe.name)
+        recipe.save()
+        return super(NewRecipe, self).form_valid(form)
+
+
+class Register(CreateView):
+    """
+    Registers an user with username and password.
+    """
+    model = User
+    template_name = 'register_form.html'
+    success_url = reverse_lazy('home')
+    fields = ['username', 'password']
+    widgets = {
+        'password': forms.PasswordInput
+    }
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        return super(Register, self).form_valid(form)
+
+    def get_form_class(self):
+        return modelform_factory(self.model, fields=self.fields, widgets=self.widgets)
+
