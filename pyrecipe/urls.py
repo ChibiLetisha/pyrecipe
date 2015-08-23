49528262b663dc1b050b11c65711b2a3dbcc0e7d
Letisha <chibi.panda93@gmail.com>
2015-08-23 14:10:12 +0200

    Login/Logout and New comment

--- pyrecipe/urls.py
+++ pyrecipe/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from recipe.views import RecipeList, RecipeDetail, Register, NewRecipe
+from recipe.views import RecipeList, RecipeDetail, Register, NewRecipe, Login, Logout, NewComment
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', RecipeList.as_view(), name='home'),
     url(r'^new-recipe/$', NewRecipe.as_view(), name='new-recipe'),
     url(r'^register/$', Register.as_view(), name='register'),
+    url(r'^login/$', Login.as_view(), name='login'),
+    url(r'^logout/$', Logout.as_view(), name='logout'),
+    url(r'^recipe/new-comment/(?P<slug>[-\w]+)/$', NewComment.as_view(), name='new-comment'),
     url(r'^recipe/(?P<slug>[-\w]+)/$', RecipeDetail.as_view(), name='recipe-detail'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,13 +1,17 @@
 from django import forms
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.views import login, logout
+from django.core.urlresolvers import reverse_lazy, reverse
 from django.forms import modelform_factory
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.utils.text import slugify
-from django.views.generic import ListView, DetailView, CreateView, View
-from recipe.models import Recipe
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.generic import ListView, DetailView, CreateView, View, FormView, RedirectView
+from recipe.models import Recipe, Comment
 
 
 class LoginRequiredMixin(View):
@@ -55,13 +59,36 @@ class NewRecipe(LoginRequiredMixin, CreateView):
         return super(NewRecipe, self).form_valid(form)
 
 
+class NewComment(LoginRequiredMixin, CreateView):
+    """
+    Saves comments posted by logged in users.
+    """
+    model = Comment
+    fields = ['comment_text']
+
+    @staticmethod
+    def get(request, *args, **kwargs):
+        raise Http404
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        recipe = Recipe.objects.get(slug=self.kwargs['slug'])
+        comment.commenter = self.request.user
+        comment.recipe = recipe
+        comment.save()
+        return super(NewComment, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('recipe-detail', kwargs={'slug': self.kwargs['slug']})
+
+
 class Register(CreateView):
     """
     Registers an user with username and password.
     """
     model = User
     template_name = 'register_form.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('login')
     fields = ['username', 'password']
     widgets = {
         'password': forms.PasswordInput
@@ -76,3 +103,37 @@ class Register(CreateView):
     def get_form_class(self):
         return modelform_factory(self.model, fields=self.fields, widgets=self.widgets)
 
+
+class Login(FormView):
+    """
+    Logs in a registered user if the username and the password is valid.
+    """
+    form_class = AuthenticationForm
+    template_name = 'login_form.html'
+
+    def form_valid(self, form):
+        redirect_to = reverse_lazy('home')
+        login(self.request, form.get_user())
+        if self.request.session.test_cookie_worked():
+            self.request.session.delete_test_cookie()
+        return HttpResponseRedirect(redirect_to)
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+
+    @method_decorator(sensitive_post_parameters('password'))
+    def dispatch(self, request, *args, **kwargs):
+        request.session.set_test_cookie()
+        return super(Login, self).dispatch(request, *args, **kwargs)
+
+
+class Logout(RedirectView):
+    """
+    Logs out the current user.
+    """
+    url = reverse_lazy('home')
+    permanent = False
+
+    def dispatch(self, request, *args, **kwargs):
+        logout(request)
+        return super(Logout, self).dispatch(request, *args, **kwargs)
